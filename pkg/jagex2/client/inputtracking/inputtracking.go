0f558cc680304cb136371d66a9ee0c2f551c0a40
zsrv <zsrv@devlan.net>
2025-12-13 00:00:00 +0000

    Add InputTracking

---
+++ pkg/jagex2/client/inputtracking/inputtracking.go
@@ -0,0 +1,264 @@
+package inputtracking
+
+import (
+	"time"
+
+	"goscape-client/pkg/jagex2/io"
+)
+
+var (
+	Enabled      bool
+	OutBuffer    *io.Packet
+	OldBuffer    *io.Packet
+	LastTime     int64
+	TrackedCount int
+	LastMoveTime int64
+	LastX        int
+	LastY        int
+)
+
+// TODO: all funcs synchronized
+
+func SetEnabled() {
+	OutBuffer = io.Alloc(1)
+	OldBuffer = nil
+	LastTime = time.Now().UnixMilli()
+	Enabled = true
+}
+
+func SetDisabled() {
+	Enabled = false
+	OutBuffer = nil
+	OldBuffer = nil
+}
+
+func Flush() *io.Packet {
+	var var1 *io.Packet
+	if OldBuffer != nil && Enabled {
+		var1 = OldBuffer
+	}
+	OldBuffer = nil
+	return var1
+}
+
+func Stop() *io.Packet {
+	var var1 *io.Packet
+	if OutBuffer != nil && OutBuffer.Pos > 0 && Enabled {
+		var1 = OutBuffer
+	}
+	SetDisabled()
+	return var1
+}
+
+func EnsureCapacity(arg1 int) {
+	if OutBuffer.Pos+arg1 >= 500 {
+		var2 := OutBuffer
+		OutBuffer = io.Alloc(1)
+		OldBuffer = var2
+	}
+}
+
+func MousePressed(arg0, arg1, arg2 int) {
+	if !Enabled || (arg0 < 0 || arg0 >= 789 || arg2 < 0 || arg2 >= 532) {
+		return
+	}
+	TrackedCount++
+	var4 := time.Now().UnixMilli()
+	var6 := (var4 - LastTime) / 10
+	if var6 > 250 {
+		var6 = 250
+	}
+	LastTime = var4
+	EnsureCapacity(5)
+	if arg1 == 1 {
+		OutBuffer.P1(1)
+	} else {
+		OutBuffer.P1(2)
+	}
+	OutBuffer.P1(int(var6))
+	OutBuffer.P3(arg0 + (arg2 << 10))
+}
+
+func MouseReleased(arg0 int) {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var2 := time.Now().UnixMilli()
+	var4 := (var2 - LastTime) / 10
+	if var4 > 250 {
+		var4 = 250
+	}
+	LastTime = var2
+	EnsureCapacity(2)
+	if arg0 == 1 {
+		OutBuffer.P1(3)
+	} else {
+		OutBuffer.P1(4)
+	}
+	OutBuffer.P1(int(var4))
+}
+
+func MouseMoved(arg0, arg2 int) {
+	if !Enabled || (arg2 < 0 || arg2 > 789 || arg0 < 0 || arg0 >= 532) {
+		return
+	}
+	var3 := time.Now().UnixMilli()
+	if var3-LastMoveTime < 50 {
+		return
+	}
+	LastMoveTime = var3
+	TrackedCount++
+	var5 := (var3 - LastTime) / 10
+	if var5 > 250 {
+		var5 = 250
+	}
+	LastTime = var3
+	if arg2-LastX < 8 && arg2-LastX >= -8 && arg0-LastY < 8 && arg0-LastY >= -8 {
+		EnsureCapacity(3)
+		OutBuffer.P1(5)
+		OutBuffer.P1(int(var5))
+		OutBuffer.P1(arg2 - LastX + 8 + (arg0 - LastY + 8<<4))
+	} else if arg2-LastX < 128 && arg2-LastX >= -128 && arg0-LastY < 128 && arg0-LastY >= -128 {
+		EnsureCapacity(4)
+		OutBuffer.P1(6)
+		OutBuffer.P1(int(var5))
+		OutBuffer.P1(arg2 - LastX + 128)
+		OutBuffer.P1(arg0 - LastY + 128)
+	} else {
+		EnsureCapacity(5)
+		OutBuffer.P1(7)
+		OutBuffer.P1(int(var5))
+		OutBuffer.P3(arg2 + (arg0 << 10))
+	}
+	LastX = arg2
+	LastY = arg0
+}
+
+func KeyPressed(arg0 int) {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var2 := time.Now().UnixMilli()
+	var4 := (var2 - LastTime) / 10
+	if var4 > 250 {
+		var4 = 250
+	}
+	LastTime = var2
+	if arg0 == 1000 {
+		arg0 = 11
+	}
+	if arg0 == 1001 {
+		arg0 = 12
+	}
+	if arg0 == 1002 {
+		arg0 = 14
+	}
+	if arg0 == 1003 {
+		arg0 = 15
+	}
+	if arg0 >= 1008 {
+		arg0 -= 992
+	}
+	EnsureCapacity(3)
+	OutBuffer.P1(8)
+	OutBuffer.P1(int(var4))
+	OutBuffer.P1(arg0)
+}
+
+func KeyReleased(arg0 int) {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var2 := time.Now().UnixMilli()
+	var4 := (var2 - LastTime) / 10
+	if var4 > 250 {
+		var4 = 250
+	}
+	LastTime = var2
+	if arg0 == 1000 {
+		arg0 = 11
+	}
+	if arg0 == 1001 {
+		arg0 = 12
+	}
+	if arg0 == 1002 {
+		arg0 = 14
+	}
+	if arg0 == 1003 {
+		arg0 = 15
+	}
+	if arg0 >= 1008 {
+		arg0 -= 992
+	}
+	EnsureCapacity(3)
+	OutBuffer.P1(9)
+	OutBuffer.P1(int(var4))
+	OutBuffer.P1(arg0)
+}
+
+func FocusGained() {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var1 := time.Now().UnixMilli()
+	var3 := (var1 - LastTime) / 10
+	if var3 > 250 {
+		var3 = 250
+	}
+	LastTime = var1
+	EnsureCapacity(2)
+	OutBuffer.P1(10)
+	OutBuffer.P1(int(var3))
+}
+
+func FocusLost() {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var1 := time.Now().UnixMilli()
+	var3 := (var1 - LastTime) / 10
+	if var3 > 250 {
+		var3 = 250
+	}
+	LastTime = var1
+	EnsureCapacity(2)
+	OutBuffer.P1(11)
+	OutBuffer.P1(int(var3))
+}
+
+func MouseEntered() {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var1 := time.Now().UnixMilli()
+	var3 := (var1 - LastTime) / 10
+	if var3 > 250 {
+		var3 = 250
+	}
+	LastTime = var1
+	EnsureCapacity(2)
+	OutBuffer.P1(12)
+	OutBuffer.P1(int(var3))
+}
+
+func MouseExited() {
+	if !Enabled {
+		return
+	}
+	TrackedCount++
+	var1 := time.Now().UnixMilli()
+	var3 := (var1 - LastTime) / 10
+	if var3 > 250 {
+		var3 = 250
+	}
+	LastTime = var1
+	EnsureCapacity(2)
+	OutBuffer.P1(13)
+	OutBuffer.P1(int(var3))
+}
